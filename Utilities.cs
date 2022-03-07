f838a64bc9528b374d7b5d645909115414f345cb
V <78761340+Synthhex@users.noreply.github.com>
2022-03-07 12:50:43 +0200

    Update Utilities.cs
    Added English language validation methods.

--- Utilities.cs
+++ Utilities.cs
@@ -1,11 +1,46 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Ciphers
 {
     class Utilities
     {
+		public Dictionary<string, bool> wordBank = new Dictionary<string, bool>();
+		public Utilities()
+		{
+			foreach (var word in File.ReadLines(@"[REDACTED]"))
+			{
+				wordBank.Add(word, true);
+			}
+		}
+		public bool IsEnglishWord(string word)
+        {
+			bool found = false;
+			return wordBank.TryGetValue(word, out found);
+        }
+		public static string GetAlphabet()
+        {
+			return "abcdefghijklmnopqrstuvwxyz,.!? :;'\"`()[]{}-";
+		}
+		public static string GenerateDictionaryKey()
+        {
+			var rand = new Random();
+			string pool = GetAlphabet();
+			StringBuilder sb = new StringBuilder("");
+			
+			while (pool.Length > 0)
+            {
+				int ind = rand.Next(pool.Length);
+				sb.Append(pool[ind]);
+				pool = pool.Replace($"{pool[ind]}", string.Empty);
+            }
+
+			return sb.ToString();
+        }
 		public static async Task<string> GenerateSentencesAsync(int sentences)
 		{
 			HttpClient cl = new HttpClient();
@@ -24,6 +59,18 @@ namespace Ciphers
 			Console.WriteLine(message);
 			Console.ForegroundColor = ConsoleColor.White;
         }
+		public static async Task NextTest()
+		{
+			Log("Testing concluded. Starting new test in 5 seconds.");
+			await Task.Delay(5000);
+			return;
+        }
+		public static async Task Conclude()
+        {
+			Log("Testing concluded. There are no more ciphers. Starting from beginning in 10 seconds.");
+			await Task.Delay(10000);
+			return;
+		}
 		public static int GCD(int a, int b)
         {
 			while (b != 0)
